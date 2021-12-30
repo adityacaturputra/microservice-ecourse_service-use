a70b9d8a4e25c5ccc544d29ea1469883baf2f41f
adityacaturputra <adityacaturputra25@gmail.com>
2021-12-30 16:48:02 +0700

    membuat api user login

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersHander = require('./handler/users');
 
 router.post('/register', usersHander.register)
+router.post('/login', usersHander.login)
 
 module.exports = router;
