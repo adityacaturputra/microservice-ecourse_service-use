62aeca84eb0292c57bc5dd434146115673b17b95
adityacaturputra <adityacaturputra25@gmail.com>
2021-12-30 22:51:13 +0700

    membuat api logout

--- routes/handler/users/index.js
+++ routes/handler/users/index.js
@@ -3,5 +3,6 @@ const login = require('./login')
 const update = require('./update')
 const getUser = require('./getUser')
 const getUsers = require('./getUsers')
+const logout = require('./logout')
 
-module.exports = {register, login, update, getUser, getUsers}
+module.exports = {register, login, update, getUser, getUsers, logout}
---
+++ routes/handler/users/logout.js
@@ -0,0 +1,24 @@
+const {User, RefreshToken} = require('../../../models')
+
+module.exports = async (req, res) => {
+    const {user_id:userId} = req.body
+    const user = await User.findByPk(userId)
+
+    if(!user) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'user not found'
+        })
+    }
+
+    await RefreshToken.destroy({
+        where: {
+            user_id: userId
+        }
+    })
+
+    return res.json({
+        status: 'success',
+        message: 'refresh token deleted' 
+    })
+}
